650398a7a71b8802095a17cf506633a5882967f4
Murilo Souza <91232437+savitarms@users.noreply.github.com>
2022-03-18 19:18:52 -0300

    Delete portfólio directory

--- portfólio/js/cursor-in-text.js
+++
@@ -1,5 +0,0 @@
-const cursor = document.querySelector(".cursor")
-document.addEventListener("mousemove", (MOVEef) => {
-    cursor.style.left = MOVEef.pageX + 'px'
-    cursor.style.top = MOVEef.pageY + 'px'
-})
