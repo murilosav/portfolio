e6ba113cc8894a41db9fb998a8666796e2e9a776
Murilo Souza <91232437+savitarms@users.noreply.github.com>
2022-05-11 16:29:17 -0300

    Atualizado

---
+++ js/scroll.js
@@ -0,0 +1,34 @@
+$("#about").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".container-about").offset().top},
+        'slow');
+});
+
+
+$("#services").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".services-container").offset().top},
+        'slow');
+});
+
+
+$("#projects").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".project-title").offset().top},
+        'slow');
+});
+
+
+$("#contact").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".contact-box").offset().top},
+        'slow');
+});
+
+
+
+$(".btn-contact").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".contact-box").offset().top},
+        'slow');
+});
